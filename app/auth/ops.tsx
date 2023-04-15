ffb658ae4008610c5cec94fcb46fd220a307bfdd
nbryleibanez <nbryleibanez@gmail.com>
2023-04-15 13:58:57 +0800

    auth

---
+++ app/auth/ops.tsx
@@ -0,0 +1,12 @@
+import { auth, app } from '../../firebaseConfig'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+
+export default function Ops(){
+
+    
+    return (
+        <div>
+            <h1>Ops</h1>
+        </div>
+    )
+}
---
+++ app/auth/page.tsx
@@ -0,0 +1,10 @@
+import Ops from './ops'
+
+export default function Auth(){
+    return (
+        <div>
+            <h1>Auth</h1>
+            <Ops />
+        </div>
+    )
+}
