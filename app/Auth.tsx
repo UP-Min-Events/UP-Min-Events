19ad9610f42b39db29fb5535f57a2bc3295f9b28
nbryleibanez <nbryleibanez@gmail.com>
2023-04-17 17:28:31 +0800

    error fix

--- app/Auth.tsx
+++ app/Auth.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 
 export default function Auth(){
     return (
-        <div className={styles.auth-rap}>
+        <div>
             <button className={styles.button}>Log in as Attendee</button>
         </div>
     )
