ebbe9e423ea2afabe4e0a3728686ab8a56f99773
nbryleibanez <nbryleibanez@gmail.com>
2023-04-19 04:43:26 +0800

    type fix attempt 2

--- app/Event.tsx
+++ app/Event.tsx
@@ -8,7 +8,7 @@ interface Props {
     time: string
 }
 
-export default function Event({ id, title, date, time }){
+export default function Event({ id, title, date, time } : Props){
     return (
         <Link className={styles.event} href={`/${id}`}>
             <h2>{title}</h2>
