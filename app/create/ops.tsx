3a0fc21ffea461251bb4096daf870f3ccc8f703c
nbryleibanez <nbryleibanez@gmail.com>
2023-04-27 13:26:42 +0800

    eslint config

--- app/create/ops.tsx
+++ app/create/ops.tsx
@@ -24,16 +24,15 @@ export default function Ops() {
 
     const dbInstance = collection(db, 'events')
     const user = auth.currentUser
-  
 
     const [page, setPage] = useState<number>(1)
 
-    const nextPage = () => {
-        setPage(page + 1)
-    }
-
     const prevPage = () => {
         setPage(page - 1)
+    }
+
+    const nextPage = () => {
+        setPage(page + 1)
     }
 
     const createEvent = async () => {
@@ -56,105 +55,33 @@ export default function Ops() {
         setEventTime('')
     }
 
-    const [currentPage, setCurrentPage] = useState(1);
-
-    const nextPage = () => setCurrentPage(currentPage + 1);
-
     return (
         <div className={inter.className}>
-            <div id="form">
+            <div >
                 <h2> Create Event</h2>
-                <div id="page1" style={{ display: currentPage === 1 ? 'block' : 'none' }}>
-                    <div>
-                        <p>Title</p>    
-                        <input
-                            type="text"
-                            value={eventName}
-                            onChange={(e) => setEventName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <p>Host</p>
-                        <input
-                            type="text"
-                            value={eventHost}
-                            onChange={(e) => setEventHost(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <p>Description</p>
-                        <input
-                            id="desc"
-                            type="text"
-                            value={eventDesc}
-                            onChange={(e) => setEventDesc(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <Button variant="text" className={inter.className} onClick={nextPage}
-                        sx={{
-                            position: 'fixed',
-                            bottom: '3rem',
-                            backgroundColor: '#a70000',
-                            color: '#fff',
-                            fontWeight: 'bold',
-                            width: '12em',
-                            borderRadius: '1em',    
-                            mx: 'auto',
-                        }}>
-                        Next
-                        </Button> 
-                    </div>  
-                </div>
-                <div id="page2" style={{ display: currentPage === 2 ? 'block' : 'none' }}>
-                    <div>
-                        <p>Date</p>
-                        <input
-                            type="date"
-                            value={eventDate}
-                            onChange={(e) => setEventDate(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <p>Time</p>
-                        <input
-                            type="time"
-                            value={eventTime}
-                            onChange={(e) => setEventTime(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <p>Venue</p>
-                        <input 
-                            type="text"
-                            value={eventVenue}
-                            onChange={(e) => setEventVenue(e.target.value)} 
-                        />
-                    </div>
-                    <div>
-                        <p>Population Limit</p>
-                        <input type="range" />
-                    </div>
-                    <div>
-                        <p>Visibility</p>
-                        <select name="" id="">
-                            <option value="public">Public</option>
-                            <option value="private">Private</option>
-                        </select>
-                    </div>
-                    <div>
-                        <p>Restrictions</p>
-                        <input type="text" />
-                    </div>
-                    <button onClick={createEvent}>Finish</button>
-                </div>
+                {page === 1 ? <Page1 nextPage={nextPage} /> : <Page2 />}
             </div>
             <div>
-                {page === 1 ? <Page1 /> : <Page2 />}
+                <button onClick={prevPage}>Back</button>
+                <Button 
+                    variant="text" 
+                    className={inter.className} 
+                    onClick={nextPage}
+                    sx={{
+                        position: 'fixed',
+                        bottom: '3rem',
+                        backgroundColor: '#a70000',
+                        color: '#fff',
+                        fontWeight: 'bold',
+                        width: '12em',
+                        borderRadius: '1em',    
+                        mx: 'auto',
+                    }}
+                >
+                    Next
+                </Button> 
+                <button onClick={createEvent}>Finish</button>
             </div>
-            <button onClick={prevPage}>Back</button>
-            <button onClick={nextPage}>Next</button>
-            <button onClick={createEvent}>Finish</button>
         </div>
     )
 }
