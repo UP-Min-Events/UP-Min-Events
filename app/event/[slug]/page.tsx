cfbb16671b8890bbe3f51717c65cc5cbcc239a54
nbryleibanez <nbryleibanez@gmail.com>
2023-05-08 01:55:19 +0800

    client display name improvements

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -1,10 +1,22 @@
 import EventClient from './EventClient'
 
+import { db } from '../../../firebaseConfig'
+import { doc, getDoc } from 'firebase/firestore'
+
+export async function generateMetadata({ params }) {
+    const docSnap = await getDoc(doc(db, 'events', `${params.slug}`))
+    const event = docSnap.data()
+
+    return {
+        title: event.name,
+        description: event.desc,
+    }
+}
+
 export default function Page({ 
     params,
- }:{
+}:{
     params: { slug: string }
- }) {
-    
+}) {
     return <EventClient id={params.slug} />
 }
