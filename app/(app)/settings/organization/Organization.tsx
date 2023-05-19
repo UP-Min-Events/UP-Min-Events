4bf4d761f49d733b8497a848fd2f5742ca24cb97
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2023-05-20 00:55:54 +0800

    Added functionality to handle edit request to Organizations

--- app/(app)/settings/organization/Organization.tsx
+++ app/(app)/settings/organization/Organization.tsx
@@ -1,3 +1,92 @@
-export default function Organization(){
-    return <div>Organization</div>
+'use client'
+
+import styles from '../page.module.css'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+import { useState, useEffect } from 'react'
+import { auth, db } from '../../../../firebaseConfig'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+
+export default function Organization() {
+     const [user] = useAuthState(auth);
+     const id = user?.uid;
+
+     const [newOrganization, setNewOrganization] = useState<string>('');
+     const [organizations, setOrganizations] = useState<string[]>([]);
+
+     const getDetails = async () => {
+          const docRef = doc(db, 'organizers', `${id}`);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+               const data = docSnap.data();
+               if (data && data.affiliatedOrganization) {
+                    setOrganizations(data.affiliatedOrganization);
+               }
+          }
+     };
+
+     useEffect(() => {
+          getDetails();
+     }, []);
+
+     const handleOrganizationChange = (index: number, value: string) => {
+          const updatedOrganizations = [...organizations];
+          updatedOrganizations[index] = value;
+          setOrganizations(updatedOrganizations);
+     };
+
+     const handleDeleteOrganization = (index: number) => {
+          const updatedOrganizations = [...organizations];
+          updatedOrganizations.splice(index, 1);
+          setOrganizations(updatedOrganizations);
+     };
+
+     const handleAddOrganization = () => {
+          if (newOrganization.trim() !== '') {
+               setOrganizations([...organizations, newOrganization]);
+               setNewOrganization('');
+          }
+     };
+
+     const handleSave = async () => {
+          const docRef = doc(db, 'organizers', `${id}`);
+          await updateDoc(docRef, { affiliatedOrganization: organizations });
+     };
+
+     return (
+          <>
+               <div className={styles.nav}>
+                    <Link href="/settings">
+                         <ArrowBackIcon sx={{ scale: '125%', color: '#a70000', p: '0' }} />
+                    </Link>
+                    <button className={styles['save-setting']} onClick={handleSave}>
+                         Save
+                    </button>
+               </div>
+               <div>Organization</div>
+               {organizations.map((org, index) => (
+                    <div key={index}>
+                         <input
+                              type="text"
+                              value={org}
+                              onChange={(e) => handleOrganizationChange(index, e.target.value)}
+                         />
+                         <button onClick={() => handleDeleteOrganization(index)}>Delete</button>
+                    </div>
+               ))}
+               <div>
+                    <input
+                         type="text"
+                         value={newOrganization}
+                         onChange={(e) => setNewOrganization(e.target.value)}
+                    />
+                    <button onClick={handleAddOrganization}>Add Organization</button>
+               </div>
+          </>
+     );
 }
---
+++ app/settings/OrganizerDetails.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link'
+
+import styles from './page.module.css'
+import { Inter } from 'next/font/google'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+const inter = Inter({ subsets: ['latin']})
+
+export default function OrganizerDetails() {
+    return (
+        <div className={`${inter.className} ${styles.infoContainer}`}>
+            <div className={styles.infoBox}>
+                <div className={styles.item}>
+                    Name
+                    <Link href="/settings/name">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+                <div className={styles.item}>
+                    College
+                    <Link href="/settings/college">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+                <div className={styles['item-bottom']}>
+                    Affliated Organization
+                    <Link href="/settings/organization">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+                <div className={styles['item-bottom']}>
+                    Account Setting
+                    <Link href="/settings/account-setting">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+            </div>
+            <div className={styles.infoBox}>
+                <div className={styles.item}>
+                    About
+                    <Link href="/settings/about">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+                <div className={styles.item}>
+                    Notifications
+                    <Link href="/settings/notifications">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+                <div className={styles['item-bottom']}>
+                    Privacy and Safety
+                    <Link href="/settings/privacy-safety">
+                        <ArrowForwardIosIcon sx={{ color: '#a70000', scale: '0.75' }}/>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
