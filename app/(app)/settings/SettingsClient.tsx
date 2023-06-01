da069935f906e9a31d341cd8d3bc1d9d340a07c3
Nhyl Bryle Ibañez <nbryleibanez@gmail.com>
2023-06-01 17:18:41 +0800

    added some safety logic

--- app/(app)/settings/SettingsClient.tsx
+++ app/(app)/settings/SettingsClient.tsx
@@ -5,28 +5,39 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import UserInfo from './UserInfo'
 import UserDetails from './UserDetails'
 import OrganizerDetails from './OrganizerDetails'
+
+import { useState } from 'react'
 import { useUserTypeContext } from '../../providers/UserTypeProvider'
 import { auth } from "../../../firebaseConfig"
 import { useRouter } from 'next/navigation'
+import CircleLoading from '@/app/(app)/loadingui/CircleLoading'
 
 export default function SettingsClient() {
 
     const { userType } = useUserTypeContext()
+    const [loading, setLoading] = useState<boolean>(false)
     const router = useRouter()
 
     const SignOut = () => {
         auth.signOut();
+        setLoading(true)
         router.push("/login")
     }
 
     return (
         <div className={styles.container}>
-            <UserInfo />
-            { userType === 'attendee' ? <UserDetails/> : <OrganizerDetails />}
-            <button className={styles['button-signout']} onClick={SignOut}>
-                <LogoutIcon />
-                Sign Out
-            </button>
+            { loading ?
+                <CircleLoading />
+                :
+                <>    
+                    <UserInfo />
+                    { userType === 'attendee' ? <UserDetails/> : <OrganizerDetails />}
+                    <button className={styles['button-signout']} onClick={SignOut}>
+                        <LogoutIcon />
+                        Sign Out
+                    </button>
+                </>
+            }
         </div>
     )
 }
