63d01e7a8b925eff633ae70462b86b251115d606
10lite <rcpaderna@up.edu.ph>
2023-06-01 20:04:27 +0800

    Adjustment of Signout button

--- app/(app)/settings/SettingsClient.tsx
+++ app/(app)/settings/SettingsClient.tsx
@@ -7,22 +7,12 @@ import OrganizerDetails from './OrganizerDetails'
 
 import { useState } from 'react'
 import { useUserTypeContext } from '@/app/providers/UserTypeProvider'
-import { auth } from "../../../firebaseConfig"
-import { useRouter } from 'next/navigation'
 import CircleLoading from '@/app/(app)/loadingui/CircleLoading'
-import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function SettingsClient() {
 
     const { userType } = useUserTypeContext()
     const [loading, setLoading] = useState<boolean>(false)
-    const router = useRouter()
-
-    const SignOut = () => {
-        auth.signOut();
-        setLoading(true)
-        router.push("/login")
-    }
 
     return (
         <div className={styles.container}>
@@ -32,11 +22,6 @@ export default function SettingsClient() {
                 <>    
                     <UserInfo />
                     { userType === 'attendee' ? <UserDetails/> : <OrganizerDetails />}
-                    <div className={styles['button-wrapper']}>
-                        <button className={styles['button-signout']} onClick={SignOut}>
-                            <LogoutIcon /> Sign Out
-                        </button>
-                    </div>
                 </>
             }
         </div>
