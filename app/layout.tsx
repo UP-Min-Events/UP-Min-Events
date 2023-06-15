9c883eea7eaeae5997ff7f56d7105d9a2da6a4dc
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2023-06-16 02:17:51 +0800

    Changed manifest file and added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from '@/app/providers/Providers'
+import Head from 'next/head'
 
 export const metadata = {
     manifest: "/manifest.json",
@@ -8,6 +9,21 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
+            <Head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link
+                    rel="icon"
+                    href="/icon?<generated>"
+                    type="image/<generated>"
+                    sizes="<generated>"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    href="/apple-icon?<generated>"
+                    type="image/<generated>"
+                    sizes="<generated>"
+                />
+            </Head>
             <Providers>
                 <body>
                     {children}
