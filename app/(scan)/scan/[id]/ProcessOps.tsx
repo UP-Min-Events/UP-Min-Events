80e878c1a9b5df28bcd7fa6d6a4e37ad21dafddd
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2023-06-15 23:16:39 +0800

    Added checkAttendedExists function

--- app/(scan)/scan/[id]/ProcessOps.tsx
+++ app/(scan)/scan/[id]/ProcessOps.tsx
@@ -7,6 +7,11 @@ import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useIsScanningContext } from '@/app/providers/IsScanningProvider'
 
+interface AttendanceDetails {
+    attendee: string;
+    dateTime: string;
+}
+
 export default function ProcessOps({ id }: { id: string }) {
 
     const [user, loading] = useAuthState(auth)
@@ -39,6 +44,8 @@ export default function ProcessOps({ id }: { id: string }) {
         return `${hours}:${minutes}`
     }
 
+
+
     const processAttendance = async (eventId: string) => {
 
         const attendee = user?.uid
@@ -49,10 +56,16 @@ export default function ProcessOps({ id }: { id: string }) {
             dateTime: getTime()
         }
 
+        const checkAttendeeExists = (attendees: AttendanceDetails[], name: string | undefined) => {
+            return attendees.some((details) => details.attendee === name);
+        };
+          
+
         if (eventDoc.exists()) {
             const attendees = eventDoc.data().attendees
+            
 
-            if (attendees.attendee.includes(attendee)) {
+            if (checkAttendeeExists(attendees, attendee)) {
                 window.alert('You are already attending this event!')
                 router.push(`/event/${eventId}`)
                 return
