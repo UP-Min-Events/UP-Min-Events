2c46d8d30468d4d9cf6da5f984d27fa53962716a
Anakin Skywalker Pactores <anakin.pactores@gmail.com>
2023-06-16 03:12:24 +0800

    Removed Head from laytout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import Providers from '@/app/providers/Providers'
-import Head from 'next/head'
 
 export const metadata = {
     manifest: "/manifest.json",
@@ -9,21 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
-            <Head>
-                <link rel="icon" href="/favicon.ico" sizes="any" />
-                <link
-                    rel="icon"
-                    href="/icon?<generated>"
-                    type="image/<generated>"
-                    sizes="<generated>"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    href="/apple-icon?<generated>"
-                    type="image/<generated>"
-                    sizes="<generated>"
-                />
-            </Head>
             <Providers>
                 <body>
                     {children}
