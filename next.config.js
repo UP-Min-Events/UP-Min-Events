26ef0221ef0717b1cc016dce08ea87f963cbdb35
Nhyl Bryle Ibañez <nbryleibanez@gmail.com>
2023-06-16 17:15:38 +0800

    photo fix

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
-        pathname: 'a/**',
+        pathname: 'fife/**',
       }
     ],
     domains: ['lh3.googleusercontent.com', 'api.qrserver.com']
